a42fc35809ab682adc75ce85149253da86dfafac
abdesslemhamani <abdesslemhamani@gmail.com>
2025-03-20 17:34:32 +0100

    test

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const sidebar = document.querySelector(".sidebar");
-const bars = document.querySelector(". fa-bars");
+const bars = document.querySelector(".fa-bars");
 const closeButton = document.querySelector(".fa-times");
 
-bars.addEventListener("click",())
+bars.addEventListener("click",() => {
+    sidebar.classList.toggle("show-sidebar");
+});
+closeButton.addEventListener("click",() => {
+ sidebar.classList.remove("show-sidebar");
+});
